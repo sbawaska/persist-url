7121b68e8fb301b3c82f671604755e6cca75aae4
Swapnil Bawaskar <sbawaskar@pivotal.io>
2018-12-09 11:47:41 -0800

    switch from jedis to lettuce client

--- src/main/java/io/projectriff/demo/persisturl/PersistUrlApplication.java
+++ src/main/java/io/projectriff/demo/persisturl/PersistUrlApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.lang.NonNull;
 
@@ -25,7 +25,7 @@ public class PersistUrlApplication {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration(
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(
                         "my-redis-master.default.svc.cluster.local"));
         // TODO parameterize this
     }
